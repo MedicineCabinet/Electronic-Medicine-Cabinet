c2960604bc880a1c2cc61e78151a2bdfa1497e3a
John Paul <jaypvilla021@gmail.com>
2024-08-23 13:20:22 +0800

    remember me functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, make_response
 
 app = Flask(__name__)
 
@@ -13,9 +13,25 @@ def login():
     if request.method == 'POST':
         username = request.form['username'] 
         password = request.form['password']
+        remember_me = 'remember_me' in request.form
+
         if username in users and users[username] == password:
             session['user'] = username  # Store the username in the session
-            return redirect(url_for('home'))
+
+            resp = make_response(redirect(url_for('home')))
+            
+            if remember_me:
+                # Set cookies to remember the username and password for 30 days
+                resp.set_cookie('username', username, max_age=30*24*60*60)
+                resp.set_cookie('password', password, max_age=30*24*60*60)
+                resp.set_cookie('remember_me', 'checked', max_age=30*24*60*60)
+            else:
+                # Clear the cookies if "Remember Me" is not checked
+                resp.delete_cookie('username')
+                resp.delete_cookie('password')
+                resp.delete_cookie('remember_me')
+                
+            return resp
         else:
             flash('Invalid username or password', 'danger')
     return render_template('login.html')
@@ -30,7 +46,12 @@ def home():
 @app.route('/logout')
 def logout():
     session.pop('user', None)
-    return redirect(url_for('login'))
+    resp = make_response(redirect(url_for('login')))
+    # Clear cookies on logout
+    resp.delete_cookie('username')
+    resp.delete_cookie('password')
+    resp.delete_cookie('remember_me')
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
